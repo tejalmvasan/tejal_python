ab9d9f4e509e5065f50b9e437e27b51c1d7de08f
tejalmvasan <tejalmvasan@gmail.com>
2022-07-29 21:27:00 +0530

    1 commit

---
+++ forloopdemo.py
@@ -0,0 +1,16 @@
+print("1st Paramater")
+for i in range (5):
+    print(i)
+
+print("2nd Paramater")
+for i in range (1,5):
+    print(i)
+
+print("3rd Paramater")
+for i in range (1,10,4):
+    print(i)
+
+print("4st Paramater")
+for i in range (5,1,-1):
+    print(i)
+
