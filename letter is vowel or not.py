83705326f62cd45d419a492f9a00cc39a4751b9b
tejalmvasan <tejalmvasan@gmail.com>
2022-07-29 21:47:50 +0530

    second commit

---
+++ letter is vowel or not.py
@@ -0,0 +1,23 @@
+'''
+Write a Python program to test whether a passed letter is a vowel or not.
+'''
+'''
+x=input("Enter Letter:")
+
+for ltr in x:
+    if ltr=='a' or ltr=='e' or ltr=='i' or ltr=='0' or ltr=='u'
+    or ltr=='A' or ltr=='E' or ltr=='I' or ltr=='O' or ltr=='U':
+        print("Enter Letter is vowel")
+    else:
+        print("Enter letter is not vowel")
+'''     
+
+
+x=input("Enter Letter:")
+
+for ltr in x:
+    if ltr.lower()=='a' or ltr.lower()=='e' or ltr.lower()=='i' or ltr.lower()=='o' or ltr.lower()=='u':
+        print("Enter Letter is vowel")
+    else:
+        print("Enter letter is not vowel")
+        
