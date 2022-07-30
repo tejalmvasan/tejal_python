9c1212cb4e56104b12dc846537884489486d371b
tejalmvasan <tejalmvasan@gmail.com>
2022-07-30 20:03:06 +0530

    1 commit

---
+++ patterns_using_forloop.py
@@ -0,0 +1,36 @@
+print("1st pattern")
+for i in range(1,6):
+    print("*" *i)
+
+print("2nd pattern")
+for i in range(1,6):
+    print(" "*(6-i),"*" *i)
+
+print("3rd pattern")
+for i in range(1,6):
+    print(" "*(6-i)," *" *i)
+
+print("4th pattern")
+for i in range(6,0,-1):
+    print(" "*(6-i)," *" *i)
+
+print("5th pattern")
+for i in range(5,0,-1):
+    print("*" *i)
+
+print("6th pattern")
+for i in range(5,0,-1):
+    print(" "*(6-i),"*" *i)
+
+print("7th pattern")
+for i in range(6,0,-1):
+    print(" "*(6-i)," *" *i)
+for i in range(1,6):
+    print(" "*(6-i)," *" *i)
+
+
+print("8th pattern")
+for i in range(1,6):
+    print(" "*(6-i)," *" *i)
+for i in range(6,0,-1):
+    print(" "*(6-i)," *" *i)
