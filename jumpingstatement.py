27b42e5071772e6141179779bfa409bd68b4b6cf
tejalmvasan <tejalmvasan@gmail.com>
2022-07-30 20:08:08 +0530

    1 commit

---
+++ jumpingstatement.py
@@ -0,0 +1,13 @@
+print("break Statement")
+for i in range(1,10):
+    if i==6:
+        break
+    else:
+        print("I : ",i)
+print("* * * * *")
+print("continue statement")
+for i in range(1,10):
+    if i==5:
+        continue
+    else:
+        print("I : ",i)
