b8a49eecbc78a7c1e29972606eb04e19a06f6aca
tejalmvasan <tejalmvasan@gmail.com>
2022-08-02 12:16:10 +0530

    stringslice

---
+++ stringslice.py
@@ -0,0 +1,21 @@
+#  01234567890123   +index
+s="python is best"
+#  43210987654321   -index
+print(s)
+print("string lengh:",len(s))
+print(s[1:11]) #ython is b
+print(s[2:]) #thon is best
+print(s[:10]) #python is
+print(s[1:11:2])#y h n i
+print(s[::4])#poss
+
+print(s[-11:-3]) #hon is b
+print(s[-10:]) #on is best
+print(s[:-6]) #python i
+print(s[-13:-2:3]) #yoib
+print(s[::-1]) #tseb si nohtyp
+
+
+
+
+
