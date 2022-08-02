ef345974378db9d0e9ad3681995e20f0e076d550
tejalmvasan <tejalmvasan@gmail.com>
2022-08-02 13:08:37 +0530

    str_count

---
+++ occurrence_in_string.py
@@ -0,0 +1,11 @@
+'''
+Write a Python program to count occurrences of a substring in a string.
+or
+Write a Python program to count the occurrences of each word in a given
+sentence
+'''
+
+s=input("enter the string : ")
+print(s)
+
+print(s.count('a'))
