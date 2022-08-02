0ccae17a5d67933c0d38f5dc7c86991ba10bf6da
tejalmvasan <tejalmvasan@gmail.com>
2022-08-02 13:05:58 +0530

    stringlenth_withoutFunction

--- stringlengthwithoutfunction.py
+++ stringlengthwithoutfunction.py
@@ -1,7 +1,10 @@
-s1=input("Enter String:")
-print(s1)
-count=0
+'''
+Write a Python program to count the number of characters (character
+frequency) in a string
+'''
 
+s1=input("enter the string : ")
+count=0
 for i in s1:
     count=count+1
-print(count)
+print("character frequency of string:",count)
