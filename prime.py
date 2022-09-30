5d6c586671db77be4524601afe3b0acafecda86e
tejalmvasan <tejalmvasan@gmail.com>
2022-09-30 12:34:55 +0530

    prime

---
+++ prime.py
@@ -0,0 +1,10 @@
+
+n=int(input("Enter the number: "))
+for num in range(2, n + 1):
+   # all prime numbers are greater than 1
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
